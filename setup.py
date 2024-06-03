12b021c404aa52a3140069038aa4a7640663c87a
Steve Hespelt <shespelt@stevens.edu>
2024-06-03 16:32:05 -0400

    bump the version from 0.3.0 to 0.3.1 - no functional change but a dependecy change due to distutils being removed from 3.12 standard library.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ Development Status :: 4 - Beta
 
 setup(
     name='tox-backticks',
-    version='0.3.0',
+    version='0.3.1',
     description='Tox plugin to allow use of backticks for subcommands',
     license='MIT',
     author_email='jayvdb@gmail.com',
